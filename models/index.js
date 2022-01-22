ce62df3bc8a3b35a6b727338ee37653b3056ab7a
chaudeveloper <ms_michanguyen@yahoo.com>
2022-01-22 14:49:32 -0500

    creating post model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
+const Post = require("./Post");
 const User = require('./User');
 
-module.exports = { User };
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+  
+module.exports = { User, Post };
