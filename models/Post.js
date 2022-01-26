5efef5999483f29d253e8f112b841241b425ac10
chaudeveloper <ms_michanguyen@yahoo.com>
2022-01-26 14:52:39 -0500

    complete application to get started for module 14

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 // create our Post model
 class Post extends Model {
   static upvote(body, models) {
@@ -21,12 +20,21 @@ class Post extends Model {
             sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
             'vote_count'
           ]
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
         ]
       });
     });
   }
 }
-
 
 // create fields/columns for Post model
 Post.init(
